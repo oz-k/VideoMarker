34c10a9e211763c2000766591d6b359b5a455012
steinsroka <62500287+steinsroka@users.noreply.github.com>
2020-06-17 23:23:39 +0900

    12
    12

---
+++ app/src/main/java/com/example/videomarker/activity/InfoActivity.java
@@ -0,0 +1,24 @@
+package com.example.videomarker.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.videomarker.R;
+
+public class InfoActivity extends AppCompatActivity {
+    private String fileUri;
+    private Button close;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+
+        Intent intent = getIntent();
+
+    }
+}
--- app/src/main/java/com/example/videomarker/holder/Holder.java
+++ app/src/main/java/com/example/videomarker/holder/Holder.java
@@ -1,25 +1,38 @@
 package com.example.videomarker.holder;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.media.Image;
+import android.provider.ContactsContract;
+import android.util.SparseBooleanArray;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.videomarker.R;
 
-public class Holder extends RecyclerView.ViewHolder {
+import java.security.AccessController;
+
+public class Holder extends RecyclerView.ViewHolder{
 
     private TextView textName;
     private TextView textDur;
     private TextView textTag;
     //private TextView textId;
+    public final ImageButton btnMore;
+
 
     public Holder(View itemView) {
         super(itemView);
         textName = (TextView) itemView.findViewById(R.id.textName);
         textDur = (TextView) itemView.findViewById(R.id.textDur);
         //textId = (TextView) itemView.findViewById(R.id.textView3);
-
+        btnMore = (ImageButton) itemView.findViewById(R.id.btnMore);
     }
 
     /*public String getId() {
